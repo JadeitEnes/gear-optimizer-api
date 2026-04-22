03fa1b14332e78ede0b1807fcdbe64b09e6f9e70
JadeitEnes <enestkr.41@hotmail.com>
2026-04-23 01:49:23 +0300

    feat: switch to PostgreSQL

--- app/database/database.py
+++ app/database/database.py
@@ -8,10 +8,13 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./gear_optimizer.db")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread":False}
-)
+if "sqlite" in DATABASE_URL:
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
